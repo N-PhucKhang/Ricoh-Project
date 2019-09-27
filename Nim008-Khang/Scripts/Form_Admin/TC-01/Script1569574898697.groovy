20876fda54849484cc0977e1ab7c07b54346f77d
N-PhucKhang <n-phuckhang@vn.pasonatech.co.jp>
2019-09-27 17:00:49 +0700

    Update TC Nim008 Date: 27/09/2019

---
+++ Nim008-Khang/Scripts/Form_Admin/TC-01/Script1569574898697.groovy
@@ -0,0 +1,23 @@
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+WebUI.callTestCase(findTestCase('Common/Login_User_Normal'), [('Username') : 'dev04', ('Password') : 'dev04'], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.navigateToUrl('http://132.145.113.198/imsl/forma/normal/view/regist_application_view/sf_nim008_apl003?', FailureHandling.STOP_ON_FAILURE)
+
+WebUI.waitForPageLoad(3)
+
+WebUI.click(findTestObject('Object Repository/Admin_Form/button_Tab2'))
+
+WebUI.delay(3)
+
+WebUI.click(findTestObject('Object Repository/Admin_Form/button_Tab2_Download_Captured_Data'))
+
+errMsg = WebUI.getText(findTestObject('QLNV_Form/label_errMsg'))
+
+WebUI.verifyEqual(errMsg , "対象がありません。チェックをしてください。")
+
+WebUI.closeBrowser()

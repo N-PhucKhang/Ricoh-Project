3d38fb6533b3cde81c302327ed295262044a98dc
TaiPTV <n-nhutai@vn.pasonatech.co.jp>
2019-10-29 10:24:19 +0700

    tab4

---
+++ Nim001-Tai/Object Repository/QLNV_Form/Tab4/clear_bt4.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>clear_bt4</name>
+   <tag></tag>
+   <elementGuidId>05fdea91-56db-4c55-a5cf-ba909cdf7f07</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;eventButton_akbtbbiclqwc&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;eventButton_akbtbbiclqwc&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>

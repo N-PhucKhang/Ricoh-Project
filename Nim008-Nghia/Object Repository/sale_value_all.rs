e3e9e5983a33d00a2e26104a0e5ab19c514f5d5e
Nghiahh <h-huunghia@vn.pasonatech.co.jp>
2019-11-05 14:34:04 +0700

    update5/11

--- Nim008-Nghia/Object Repository/sale_value_all.rs
+++ Nim008-Nghia/Object Repository/sale_value_all.rs
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>sale_value_equal_26</name>
+   <name>sale_value_all</name>
    <tag></tag>
    <elementGuidId>c59f9e9d-374e-415c-ba7f-f7521ddadc50</elementGuidId>
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value>//td[@title=&quot;26&quot;]</value>
+         <value>//*[@id=&quot;cb_gt9&quot;]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -17,6 +17,6 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>//td[@title=&quot;26&quot;]</value>
+      <value>//*[@id=&quot;cb_gt9&quot;]</value>
    </webElementProperties>
 </WebElementEntity>
